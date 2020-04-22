79f4351a017ddcd5d2ad52a755b264a070d379e8
Justas Eimutis <justas.eimutis@devbridge.com>
2020-04-22 12:54:16 +0300

    fix popup

--- inProgress.js
+++ inProgress.js
@@ -1,8 +1,9 @@
 {
 const fn = async () => {
 		const delay = ms => new Promise(res => setTimeout(res, ms));
+		console.log(document.querySelector('[data-test-id="issue.views.issue-base.foundation.status.status-field-wrapper"]'));
 document.querySelector('[data-test-id="issue.views.issue-base.foundation.status.status-field-wrapper"]').querySelector('button').click();
-await delay(200);
+await delay(400);
 document.querySelector("[id$='-option-5']")?.click();
 await delay(1000);
 document.querySelector('[data-test-id="issue.views.issue-base.foundation.status.status-field-wrapper"]').querySelector('button').click();
--- popup.js
+++ popup.js
@@ -6,15 +6,6 @@ console.log("help");
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
- assign.onclick = async (element) => {
-	console.log("clicked");
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.querySelector("#assign-to-me").click();'});
-    });
-  };
   
   
    progress.onclick = async (element) => {
