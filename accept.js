18b4822ba4aa5ac9b2d32b9b57361b2102088ea5
Justas Eimutis <justas.eimutis@devbridge.com>
2020-07-16 13:36:37 +0300

    fix code review -> accepted path

--- accept.js
+++ accept.js
@@ -3,7 +3,7 @@
     const delay = ms => new Promise(res => setTimeout(res, ms));
 	const openid= '[data-test-id="issue.views.issue-base.foundation.status.status-field-wrapper"]';
     const buttons = [
-    "[id$='-option-3']","[id$='-option-3']", "[id$='-option-2']", "[id$='-option-4']", "[id$='-option-3']"
+    "[id$='-option-3']","[id$='-option-3']", "[id$='-option-4']", "[id$='-option-3']"
     ];
 document.querySelector(openid).querySelector('button').click();
 await delay(400);
@@ -20,10 +20,6 @@ await delay(1500);
 document.querySelector(openid).querySelector('button').click();
 await delay(200);
      document.querySelector(buttons[3])?.click();
-await delay(1500);
-document.querySelector(openid).querySelector('button').click();
-await delay(200);
-     document.querySelector(buttons[4])?.click();
 }
 
 accept();
